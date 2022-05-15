7fb0ddf6ff562532d2cab07dcb54fee7835aa20c
PrashantP <prashantpatel622@gmail.com>
2022-05-14 23:11:30 -0400

    set up

---
+++ src/components/Nav/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Nav(){
+    return (
+      <header>
+          <h2>
+            <a href="/">
+                Prashant
+            </a>
+          </h2>
+            <nav>
+                <ul>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Projects
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        Resume
+                    </li>
+                </ul>
+            </nav> 
+      </header>  
+    );
+}
+
+export default Nav;
