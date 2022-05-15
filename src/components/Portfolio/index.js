70c0f91ca8e8d673fbc68c774a4fac67154267e3
PrashantP <prashantpatel622@gmail.com>
2022-05-15 18:11:17 -0400

    start portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Projects from "../Projects";
 
 function Portfolio() {
     return(
         <section id="Work" class="one">
-            <h2 class="section-title">Work</h2>
-            <div class="works">
-
-            </div>
-        </section>
+        <h2 class="section-title">Work</h2>
+        <Projects/>
+    </section>
     )
 }
 
---
+++ src/components/Projects/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Projects = () => {
+    return(
+        <div class="works">
+        <a href="https://prashantp22.github.io/potterquiz/">
+            <article class="work">
+                <img src= {require(`../Projects/potter.jpg`)} alt="" />
+                <div class="workname">
+                    <h3>Potter Quiz</h3>
+                    <h4>HTML/CSS?JavaScript</h4>
+                </div>
+            </article>
+        </a>
+
+        <a>
+            <article class="work">
+                <img src={require(`../Projects/dealership.png`)} alt="" />
+                <div class="workname">
+                    <h3>Dealership Inventory</h3>
+                    <h4>Node</h4>
+                </div>
+            </article>
+        </a>
+
+        <a href="https://adunny.github.io/concert-vagabond/">
+            <article class="work">
+                <img src={require(`../Projects/concert-vagabond.jpg`)} alt="" />
+                <div class="workname">
+                    <h3>Concert Vagabond</h3>
+                    <h4>HTML/JavaScript/CSS/jQuery/Materialize</h4>
+                </div>
+            </article>
+        </a>
+
+        <a href="https://prashantp22.github.io/weather-dashboard/">
+            <article class="work">
+                <img src={require(`../Projects/weather.jpg`)} alt="" />
+                <div class="workname">
+                    <h3>Weather Dashboard</h3>
+                    <h4>JavaScript</h4>
+                </div>
+            </article>
+        </a>
+
+        <a href="https://prashantp22.github.io/run-buddy/">
+            <article class="work">
+                <img src={require(`../Projects/runbuddy.jpg`)} alt="" />
+                <div class="workname">
+                    <h3>Run Buddy</h3>
+                    <h4>HTML/CSS</h4>
+                </div>
+            </article>
+        </a>
+    </div>
+    );
+ 
+}
+
+export default Projects;
